a78f4aa1d8bfe7c3cab233f272bbc391bd9e2ffc
yyyqyyy <yyyqyyy@hotmail.com>
2017-09-28 09:37:15 +0800

    11111

--- canvas_circle_drawing/js/html5games.untangle.js
+++ canvas_circle_drawing/js/html5games.untangle.js
@@ -4,6 +4,7 @@
 var untangleGame = {
     circles: [],
     thinLineThickness: 1,
+    boldLineThickness: 5,
     lines: []
 };
 
@@ -14,9 +15,56 @@ function Circle(x, y, radius) {
 }
 
 function Line(startPoint, endPoint, thickness) {
-    this.tartPoint = startPoint;
+    this.startPoint = startPoint;
     this.endPoint = endPoint;
     this.thickness = thickness;
+}
+
+function isInBetween(a, b, c) {
+    if (Math.abs(a - b) < 0.000001 || Math.abs(b - c) < 0.000001) {
+        return false;
+    }
+    return ((a < b && b < c) || (c < b && b < a));
+}
+
+function isIntersect(line1, line2) {
+    var a1 = line1.endPoint.y - line1.startPoint.y;
+    var b1 = line1.startPoint.x - line1.endPoint.x;
+    var c1 = a1 * line1.startPoint.x + b1 * line1.startPoint.y;
+
+    var a2 = line2.endPoint.y - line2.startPoint.y;
+    var b2 = line2.startPoint.x - line2.endPoint.x;
+    var c2 = a2 * line2.startPoint.x + b2 * line2.startPoint.y;
+
+    var d = a1 * b2 - a2 * b1;
+
+    if (d == 0) {
+        return false;
+    } else {
+        var x = (b2 * c1 - b1 * c2) / d;
+        var y = (a1 * c2 - a2 * c1) / d;
+
+        if ((isInBetween(line1.startPoint.x, x, line1.endPoint.x) ||
+            isInBetween(line1.startPoint.y, y, line1.endPoint.y)) &&
+            (isInBetween(line2.startPoint.x, x, line2.endPoint.x) ||
+            isInBetween(line2.startPoint.y, y, line2.endPoint.y))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateLineIntersection() {
+    for (var i = 0; i < untangleGame.lines.length; i++) {
+        for (var j = 0; j < i; j++) {
+            var line1 = untangleGame.lines[i];
+            var line2 = untangleGame.lines[j];
+            if (isIntersect(line1, line2)) {
+                line1.thickness = untangleGame.boldLineThickness;
+                line2.thickness = untangleGame.boldLineThickness;
+            }
+        }
+    }
 }
 
 function drawLine(ctx, x1, y1, x2, y2, thickness) {
@@ -45,6 +93,7 @@ function connectCircles() {
             untangleGame.lines.push(new Line(startPoint, endPoint, untangleGame.thinLineThickness));
         }
     }
+    updateLineIntersection();
 }
 
 function clear(ctx, canvas) {
@@ -149,7 +198,7 @@ function gameLoop() {
 
     for (var li = 0; li < untangleGame.lines.length; li++) {
         var line = untangleGame.lines[li];
-        var startPoint = line.tartPoint;
+        var startPoint = line.startPoint;
         var endPoint = line.endPoint;
         var thickness = line.thickness;
         drawLine(ctx, startPoint.x, startPoint.y, endPoint.x, endPoint.y, thickness);
